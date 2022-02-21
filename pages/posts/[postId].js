20967b2bc66a6449bcb2cdd022142f956c90a1db
Charlie-007-star <sujinsr2457@gmail.com>
2022-02-21 15:46:57 +0530

    routing done

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,11 @@
+import { useRouter } from "next/router";
+
 const Post = ({ post }) => {
+    const router = useRouter();
+
+    if(router.isFallback){
+        <h1>Loading...</h1>
+    }
     return (
         <>
         <h1>Single Post - {post.id} {post.title}</h1>
@@ -9,12 +16,38 @@ const Post = ({ post }) => {
 
 export default Post;
 
+/* ------------------------------ Static paths ------------------------------ */
+export async function getStaticPaths() {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    const paths = data.map((post) => {
+        return {
+            params: {
+                postId: `${post.id}`
+            }
+        }
+    })
+
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+/* ------------------------------ Static Props ------------------------------ */
 export async function getStaticProps(context) {  
 
     const { params } =  context;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await response.json();
-    console.log(data);
+
+    if(!data.id){
+        return {
+            notFound:true
+        }
+    }
+
     return {
         props: {   
             post: data,
