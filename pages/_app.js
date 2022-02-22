44371b341bed9f34900aeb1166401eda59fadca9
Charlie-007-star <sujinsr2457@gmail.com>
2022-02-22 09:58:50 +0530

    react query with next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import {QueryClientProvider,QueryClient} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import '../styles/globals.css';
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+   <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+  );
 }
 
 export default MyApp;
